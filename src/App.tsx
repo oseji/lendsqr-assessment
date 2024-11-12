fdf460fbb81a9628071e6067ecdba9dd9180f76e
oseji <jiade1233@gmail.com>
2024-11-12 19:38:54 +0100

    completed login page for desktop screens

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LoginPage from "./LoginPage";
+
+function App() {
+  return (
+    <div id="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LoginPage.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+import lendSqrLogo from "./assets/Logo.svg";
+import loginPageImage from "./assets/pablo-sign-in 1.png";
+
+const LoginPage = () => {
+  const [passwordSpan, setPasswordSpan] = useState<"show" | "hide">("show");
+
+  return (
+    <div id="loginPage">
+      <section>
+        <div className="sectionGroups">
+          <img src={lendSqrLogo} alt="Lendsqr's logo" className="logo" />
+
+          <img src={loginPageImage} alt="image" className="loginPageImage" />
+        </div>
+
+        <form
+          onSubmit={(event) => event.preventDefault()}
+          className="sectionGroups"
+        >
+          <div>
+            <h1>Welcome!</h1>
+
+            <p style={{ marginBottom: "60px" }}>Enter details to login.</p>
+
+            <div className="loginInputGroup">
+              <input
+                type="email"
+                inputMode="email"
+                name="emailAddress"
+                id="emailAddress"
+                placeholder="Email"
+                required={true}
+              />
+            </div>
+
+            <div className="loginInputGroup">
+              <input
+                type="text"
+                name="password"
+                id="password"
+                placeholder="Password"
+                required={true}
+              />
+
+              <span
+                style={{
+                  color: "#39CDCC",
+                  textTransform: "uppercase",
+                  fontSize: "0.75rem",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  passwordSpan === "show"
+                    ? setPasswordSpan("hide")
+                    : setPasswordSpan("show");
+                }}
+              >
+                {passwordSpan}
+              </span>
+            </div>
+
+            <p
+              style={{
+                color: "#39CDCC",
+                fontSize: "0.75rem",
+                marginBottom: "30px",
+                cursor: "pointer",
+              }}
+            >
+              FORGOT PASSWORD?
+            </p>
+
+            <button>LOG IN</button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default LoginPage;
